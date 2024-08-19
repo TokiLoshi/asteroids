40fd12c0fef82d2adf8ef48704c2a88083297e60
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-08-19 15:04:37 -0700

    added grouping

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
   pygame.get_init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
+
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
 
@@ -13,10 +18,14 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return
-   
-    player.update(dt)
+    for item in updatable:
+      item.update(dt)
+
     screen.fill("black")
-    player.draw(screen)
+    
+    for item in drawable:
+      item.draw(screen)
+    
     
     pygame.display.flip()
 
